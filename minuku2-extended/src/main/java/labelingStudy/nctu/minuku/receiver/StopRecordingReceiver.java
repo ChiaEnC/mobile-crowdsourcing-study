1e73248b33bc4489465d29091628327187f3438e
ChiaEnChiang <vivian123.cs02@gmail.com>
2019-05-22 09:19:45 +0200

    modify recording display

---
+++ minuku2-extended/src/main/java/labelingStudy/nctu/minuku/receiver/StopRecordingReceiver.java
@@ -0,0 +1,10 @@
+package labelingStudy.nctu.minuku.receiver;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by chiaenchiang on 28/04/2019.
+ */
+
+public class StopRecordingReceiver extends BroadcastReceiver {
+}
